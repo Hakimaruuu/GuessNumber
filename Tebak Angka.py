855c405fc0c0e3d50426a45d5b9194176a580927
Hakimaruuu <57005303+Hakimaruuu@users.noreply.github.com>
2020-03-22 23:16:02 +0700

    Update Tebak Angka.py

--- Tebak Angka.py
+++ Tebak Angka.py
@@ -8,65 +8,80 @@ Created on Thu Mar 19 10:16:58 2020
 import random;
 
 main = 'y'
-start = 1
-end = 1000
 jawab = 'n'
-bawah = start
-atas = end
+angka = 1000
+coba = 1
+run = 0
+model = 0
+mod = [1000,500,125,63,32,16,8,4,2,1]
 
-def processing(jawab,atas,bawah,angka):
+
+def processing(jawab,iterate,angka,run,model):
     if jawab == 'a':
-        if angka > bawah :
-            bawah = angka + 1;
-            
-        angka = random.randint(bawah,atas)
+        angka = angka + mod[iterate]
+        iterate = iterate + 1
         
-    if jawab == 'b':
-        if angka < atas :
-            atas = angka - 1;
-        angka = random.randint(bawah,atas)
+        if angka == 1000:
+            model = 0
+        elif angka == 0:
+            model = 2
+        elif angka != 0 and angka != 1000:
+            model = 1
+            
+
+        if coba == 10:
+            run = 1
     
-    return angka,atas,bawah;
+    if jawab == 'b':
+        angka = angka - mod[iterate]
+        iterate = iterate + 1
+        
+        if angka == 0:
+            model = 2
+        elif angka == 0:
+            model = 1
+        elif angka != 0 and angka != 1000:
+            model = 1
+            
+            
+        if coba == 10:
+            run = 1
+            
+    return angka, iterate, run, model;
 
 while main == 'y':
-    bawah = start
-    atas = end
-    angka = random.randint(bawah,atas)
-    i = 0
+    i = 1
     jawab = 'n'
     
-    while jawab != 'y':
+    while i<10 :
+        angka, i, run, model = processing(jawab,i,angka,run,model)
+        
         print("")
         print("------------------------------------------------------------------")
         print("")
-        print("Iterasi ke-",i+1)
+        print("Iterasi ke-",i)
         print("")
         print('Pikirkan angka dari 1 sampai 1000 :',angka)
-        jawab = input('Apakah angka anda lebih (a), kurang (b), benar (y) ? : ')
+        print("model : ",model)
         
+        
+        
+        if model == 0:
+            jawab = input('Apakah angka anda kurang (b), benar (y) ? : ')
+        elif model == 1:
+            jawab = input('Apakah angka anda lebih (a), kurang (b), benar (y) ? : ')
+        elif model == 2:
+            jawab = input('Apakah angka anda lebih (a), benar (y) ? : ')
+        
+        
+        
+        if run == 1:
+            break
         if jawab == 'y':
             break
-        
-        if angka+1==bawah or angka-1==atas:
-            print("")
-            print("Angkanya pasti",atas)
-            break
-        
-        if angka==1 :
-            print("")
-            print("Angkanya pasti",angka,"orang batas bawahnya 1 kok")
-            break
-        elif angka==999 :
-            print("")
-            print("Angkanya pasti",angka,"orang batasnya 1000 ga ikutan kok")
-            break
-        else :
-            angka, atas, bawah = processing(jawab,atas,bawah,angka)
-        
-        i = i + 1;   
     else :
         print("")
-        print("Nyerah saya")
+        print("Anda pasti berbohong")
     print("")
     print("HAHAHA") 
-    main = input("Lanjut? (y/n) :")
+    main = input("Lanjut? (y/n) : ")
